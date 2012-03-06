b98266795132eb543bf0c02348e3d7e8825d3647
Chuck Mortimore <cmortimore@salesforce.com>
2012-03-05 20:43:39 -0800

    changed message

--- src/main/java/com/force/api/demo/EventMapper.java
+++ src/main/java/com/force/api/demo/EventMapper.java
@@ -16,7 +16,7 @@ public class EventMapper extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        resp.getWriter().print("Hello from Java!\n");
+        resp.getWriter().print("EventMapper");
     }
 
     public static void main(String[] args) throws Exception{
